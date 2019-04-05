1a5f4d123819cffd78c547890e504e108e892aa4
Lars Lindberg <lars.lindberg@xlent.se>
2019-04-05 11:18:19 +0200

    Web: 1.7.9 Better exception handling in handler ThrowFulcrumExceptionOnFail.

--- src/Libraries.Web/Pipe/Outbound/ThrowFulcrumExceptionOnFail.cs
+++ src/Libraries.Web/Pipe/Outbound/ThrowFulcrumExceptionOnFail.cs
@@ -2,6 +2,9 @@
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Rest;
+using Nexus.Link.Libraries.Core.Application;
+using Nexus.Link.Libraries.Core.Assert;
 using Nexus.Link.Libraries.Core.Error.Logic;
 using Nexus.Link.Libraries.Core.Logging;
 using Nexus.Link.Libraries.Web.Error.Logic;
@@ -14,12 +17,38 @@ namespace Nexus.Link.Libraries.Web.Pipe.Outbound
     /// </summary>
     public class ThrowFulcrumExceptionOnFail : DelegatingHandler
     {
+
+        /// <summary>
+        /// A delegate for testing purposes
+        /// </summary>
+        /// <param name="request"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public delegate Task<HttpResponseMessage> SendAsyncDelegate(HttpRequestMessage request, CancellationToken cancellationToken);
+
+        /// <summary>
+        /// Use this in unit tests to inject code that will be called to simulate SendAsync().
+        /// </summary>
+        // ReSharper disable once InconsistentNaming
+        public SendAsyncDelegate UnitTest_SendAsyncDependencyInjection { get; set; }
+
         /// <inheritdoc />
         protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
         {
             try
             {
-                var response = await base.SendAsync(request, cancellationToken);
+                HttpResponseMessage response;
+                if (UnitTest_SendAsyncDependencyInjection == null)
+                {
+                    response = await base.SendAsync(request, cancellationToken);
+                }
+                else
+                {
+                    // This is for unit testing
+                    FulcrumAssert.IsTrue(FulcrumApplication.IsInDevelopment);
+                    response = await UnitTest_SendAsyncDependencyInjection(request, cancellationToken);
+                }
+
                 var fulcrumException = await ExceptionConverter.ToFulcrumExceptionAsync(response);
                 if (fulcrumException == null) return response;
                 Log.LogInformation(
@@ -30,9 +59,19 @@ namespace Nexus.Link.Libraries.Web.Pipe.Outbound
             {
                 throw;
             }
-            catch (Exception exception)
+            catch (TaskCanceledException e)
             {
-                throw new FulcrumAssertionFailedException($"Request {request.ToLogString()} failed with exception of type {exception.GetType()}.", exception);
+                var message = $"HTTP request {request.ToLogString()} was cancelled.";
+                Log.LogWarning(message, e);
+                throw new FulcrumTryAgainException(message, e);
+            }
+            catch (Exception e)
+            {
+                // If we end up here, we probably need to add another catch statement for that exception type.
+                var message = $"Request {request.ToLogString()} failed with exception of type {e.GetType()}. Please report that the outbound pipe handler {nameof(ThrowFulcrumExceptionOnFail)} should catch this exception type explicitly.";
+                Log.LogError(message, e);
+                throw new FulcrumAssertionFailedException(
+                    message, e);
             }
         }
     }
