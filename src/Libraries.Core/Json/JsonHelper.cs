27feb865da7895bdd82c3a4c8b49837b9746448e
Lars Lindberg <lars.lindberg@xlent.se>
2019-06-26 11:14:29 +0200

    Started bugfix for json convert.

---
+++ src/Libraries.Core/Json/JsonHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using Nexus.Link.Libraries.Core.Error.Model;
+
+namespace Nexus.Link.Libraries.Core.Json
+{
+    /// <summary>
+    /// Convenience methods for JSON stuff.
+    /// </summary>
+    public class JsonHelper
+    {
+        public static bool TryDeserializeObject<T>(string value, out T deserializedObject)
+        {
+            deserializedObject = null;
+            var success = false;
+            try
+            {
+                deserializedObject = JsonConvert.DeserializeObject<FulcrumError>();
+            }
+        }
+    }
+}
