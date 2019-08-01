2a585cf401c47c1769285adb48b18f44cd3f031c
Lars Lindberg <lars.lindberg@xlent.se>
2019-08-01 12:17:31 +0200

    Services.Controllers: 1.1.0 Introduced RootsControllerBase

---
+++ src/Services.Controllers/Capabilities/Api/IRootService.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Nexus.Link.Services.Controllers.Capabilities.Api
+{
+    public interface IRootService
+    {
+        Task<ContentResult> Welcome(CancellationToken token = default(CancellationToken));
+    }
+}
