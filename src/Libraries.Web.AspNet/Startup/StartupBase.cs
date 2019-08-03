6f863547ec7577ae8dcb3664978d237c4ef49a49
Lars Lindberg <lars.lindberg@xlent.se>
2019-08-03 11:23:48 +0200

    Web.AspNet: 2.5.2 Refactored StartupHelperBase.

--- src/Libraries.Web.AspNet/Startup/StartupBase.cs
+++ src/Libraries.Web.AspNet/Startup/StartupBase.cs
@@ -12,7 +12,6 @@ using Nexus.Link.Libraries.Core.Application;
 using Nexus.Link.Libraries.Core.Assert;
 using Nexus.Link.Libraries.Core.Logging;
 using Nexus.Link.Libraries.Web.AspNet.Pipe.Inbound;
-using Nexus.Link.Libraries.Web.Platform.Authentication;
 using Swashbuckle.AspNetCore.Swagger;
 
 namespace Nexus.Link.Libraries.Web.AspNet.Startup
@@ -20,7 +19,7 @@ namespace Nexus.Link.Libraries.Web.AspNet.Startup
     /// <summary>
     /// Helper class for the different steps in the Startup.cs file.
     /// </summary>
-    public abstract class StartupHelperBase : IValidatable
+    public abstract class StartupBase : IValidatable
     {
         /// <summary>
         /// The configuration from the Startup constructor.
@@ -37,7 +36,7 @@ namespace Nexus.Link.Libraries.Web.AspNet.Startup
         /// </summary>
         /// <param name="configuration">Take this from the Startup constructor.</param>
         /// <param name="isBusinessApi">Should be false for adapters and true for the business API.</param>
-        protected StartupHelperBase(IConfiguration configuration, bool isBusinessApi)
+        protected StartupBase(IConfiguration configuration, bool isBusinessApi)
         {
             Configuration = configuration;
             IsBusinessApi = isBusinessApi;
@@ -48,62 +47,64 @@ namespace Nexus.Link.Libraries.Web.AspNet.Startup
         /// </summary>
         /// <remarks>
         /// Will be used to generate other technical names like the file for Swagger documentation.</remarks>
-        public string TechnicalName { get; set; }
+        protected string TechnicalName { get; set; }
 
         /// <summary>
         /// The version of the API. To be used in the Open API (swagger).
         /// </summary>
-        public virtual string ApiVersion { get; set; } = "v1";
+        protected virtual string ApiVersion { get; set; } = "v1";
 
         /// <summary>
         /// The .NET Core version. Defaults to the latest.
         /// </summary>
-        public virtual CompatibilityVersion CompatibilityVersion { get; set; } = CompatibilityVersion.Latest;
-
-
-        /// <summary>
-        /// The name of the XML file. Created based on the <see cref="TechnicalName"/>.
-        /// </summary>
-        /// <returns>The path or NULL if no file is found at that path.</returns>
-        public virtual string GetCommentsFilePath()
-        {
-            var xmlFile = $"{TechnicalName}.Service.xml";
-            var path = Path.Combine(AppContext.BaseDirectory, xmlFile);
-            if (File.Exists(path)) return path;
-
-            Log.LogWarning($"Could not find API documentation file ({path})");
-            return null;
-        }
+        protected virtual CompatibilityVersion CompatibilityVersion { get; set; } = CompatibilityVersion.Latest;
 
         /// <summary>
-        /// Use this method in the Startup.ConfigureServices method and use the parameter from that method.
+        /// This method will be called indirectly from the program's Main method to configure the services
         /// </summary>
+        /// <remarks>
+        /// Don't override this method unless you really know what you are doing.
+        /// First see if the following methods could be good enough for your needs:
+        /// Always override <see cref="GetSynchronousFastLogger"/> to establish your preferred way of logging.
+        /// Always override <see cref="DependencyInjectServices"/> to inject your own services.
+        /// Override <see cref="ConfigureServicesInitialUrgentPart"/> if you have things that needs to be initialized early.
+        /// Override <see cref="ConfigureServicesSwagger"/> if you want to change how swagger is set up.
+        /// </remarks>
         public virtual void ConfigureServices(IServiceCollection services)
         {
             try
             {
-                InitialServiceConfiguration(services);
+                ConfigureServicesInitialUrgentPart(services);
+                FulcrumApplication.ValidateButNotInProduction();
+                InternalContract.RequireValidated(this, GetType().FullName);
+                services.AddMvc().SetCompatibilityVersion(CompatibilityVersion);
+                ConfigureServicesSwagger(services);
                 DependencyInjectServices(services);
-                Log.LogInformation($"{nameof(StartupHelperBase)}.{nameof(ConfigureServices)} succeeded.");
+                Log.LogInformation($"{nameof(StartupBase)}.{nameof(ConfigureServices)} succeeded.");
             }
             catch (Exception e)
             {
                 Log.LogCritical(
-                    $"{nameof(StartupHelperBase)}.{nameof(ConfigureServices)} failed. Application {TechnicalName} needs to be restarted.",
+                    $"{nameof(StartupBase)}.{nameof(ConfigureServices)} failed. The application {TechnicalName} needs to be restarted.: {e.Message}",
                     e);
                 throw;
             }
         }
 
         /// <summary>
-        /// Use this method in the Startup.Configure method and use the parameters from that method.
+        /// This method will be called indirectly from the program's Main method to configure the services
         /// </summary>
-        public virtual void ConfigureMiddleware(IApplicationBuilder app, IHostingEnvironment env)
+        /// <remarks>
+        /// Don't override this method unless you really know what you are doing.
+        /// First see if the following methods could be good enough for your needs:
+        /// Override <see cref="ConfigureSwaggerMiddleware"/> if you want to change how swagger is set up.
+        /// </remarks>
+        public virtual void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             try
             {
+                ConfigureAppMiddleware(app, env);
                 InternalContract.RequireValidated(this, GetType().FullName);
-                ConfigureSwaggerMiddleware(app, env);
                 if (env.IsDevelopment())
                 {
                     app.UseDeveloperExceptionPage();
@@ -113,15 +114,12 @@ namespace Nexus.Link.Libraries.Web.AspNet.Startup
                     app.UseHsts();
                 }
                 app.UseHttpsRedirection();
-                ConfigureNexusLinkMiddleware(app, env);
-                ConfigureBusinessApiMiddleware(app, env);
-                ConfigureAppMiddleware(app, env);
                 app.UseMvc();
-                Log.LogInformation($"{nameof(StartupHelperBase)}.{nameof(ConfigureMiddleware)} succeeded.");
+                Log.LogInformation($"{nameof(StartupBase)}.{nameof(Configure)} succeeded.");
             }
             catch (Exception e)
             {
-                Log.LogCritical($"{nameof(StartupHelperBase)}.{nameof(ConfigureMiddleware)} failed. Application {TechnicalName} needs to be restarted.", e);
+                Log.LogCritical($"{nameof(StartupBase)}.{nameof(Configure)} failed. The application {TechnicalName} needs to be restarted.: {e.Message}", e);
                 throw;
             }
         }
@@ -134,27 +132,28 @@ namespace Nexus.Link.Libraries.Web.AspNet.Startup
         /// Assumes that there is a "FulcrumApplication" section in the app settings file.
         /// </remarks>
         /// <param name="services">The parameter from Startup.ConfigureServices.</param>
-        protected virtual void InitialNexusLinkConfiguration(IServiceCollection services)
+        protected virtual void ConfigureServicesInitialUrgentPart(IServiceCollection services)
         {
             var configurationSection = Configuration.GetSection("FulcrumApplication");
             FulcrumAssert.IsNotNull(configurationSection);
             Application.FulcrumApplicationHelper.WebBasicSetup(configurationSection);
+            TechnicalName = FulcrumApplication.AppSettings.GetString("TechnicalName", true);
+            FulcrumApplication.Setup.SynchronousFastLogger = GetSynchronousFastLogger();
         }
 
         /// <summary>
-        /// Initial settings that is needed for your environment
+        /// Get the logger that should be use for logging for this application. The logger must implement <see cref="ISyncLogger"/>.
         /// </summary>
-        /// <param name="services">The parameter from Startup.ConfigureServices.</param>
-        protected virtual void InitialLocalConfiguration(IServiceCollection services)
-        {
-            TechnicalName = FulcrumApplication.AppSettings.GetString("TechnicalName", true);
-        }
+        /// <remarks>
+        /// If you have a logger that implements <see cref="IAsyncLogger"/> you can make it an <see cref="ISyncLogger"/> by adding .AsSyncLogger after your instance.
+        /// </remarks>
+        protected abstract ISyncLogger GetSynchronousFastLogger();
 
         /// <summary>
         /// Configure Swagger
         /// </summary>
         /// <param name="services">From the parameter to Startup.ConfigureServices.</param>
-        protected virtual void InitialSwaggerConfiguration(IServiceCollection services)
+        protected virtual void ConfigureServicesSwagger(IServiceCollection services)
         {
             services.AddSwaggerGen(c =>
             {
@@ -177,25 +176,20 @@ namespace Nexus.Link.Libraries.Web.AspNet.Startup
             });
         }
 
-        /// <summary>
-        /// Injects the services from Nexus.Services.Implementation
-        /// </summary>
-        /// <param name="services">From the parameter to Startup.ConfigureServices.</param>
-        protected abstract void DependencyInjectNexusServices(IServiceCollection services);
 
         /// <summary>
-        /// This is where the business API injects its service SDK for the adapters.
+        /// The name of the XML file. Created based on the <see cref="TechnicalName"/>.
         /// </summary>
-        /// <param name="services">From the parameter to Startup.ConfigureServices.</param>
-        protected virtual void DependencyInjectBusinessApiServices(IServiceCollection services)
+        /// <returns>The path or NULL if no file is found at that path.</returns>
+        protected virtual string GetCommentsFilePath()
         {
-        }
+            var xmlFile = $"{TechnicalName}.Service.xml";
+            var path = Path.Combine(AppContext.BaseDirectory, xmlFile);
+            if (File.Exists(path)) return path;
 
-        /// <summary>
-        /// This is where the application injects its own services.
-        /// </summary>
-        /// <param name="services">From the parameter to Startup.ConfigureServices.</param>
-        protected abstract void DependencyInjectAppServices(IServiceCollection services);
+            Log.LogWarning($"Could not find API documentation file ({path})");
+            return null;
+        }
 
         /// <summary>
         /// Convenience method to catch dependency injection exceptions and log them as critical errors.
@@ -204,7 +198,7 @@ namespace Nexus.Link.Libraries.Web.AspNet.Startup
         /// <param name="provider"></param>
         /// <param name="dependencyInjection"></param>
         /// <returns></returns>
-        public virtual T ValidateDependencyInjection<T>(IServiceProvider provider,
+        protected virtual T ValidateDependencyInjection<T>(IServiceProvider provider,
             Func<IServiceProvider, T> dependencyInjection)
         {
             try
@@ -218,27 +212,12 @@ namespace Nexus.Link.Libraries.Web.AspNet.Startup
             }
         }
 
-        private void InitialServiceConfiguration(IServiceCollection services)
-        {
-            InitialNexusLinkConfiguration(services);
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion);
-            InitialLocalConfiguration(services);
-            InitialSwaggerConfiguration(services);
-            InternalContract.RequireValidated(this, GetType().FullName);
-        }
-
-        private void DependencyInjectServices(IServiceCollection services)
-        {
-            if (IsBusinessApi)
-            {
-                DependencyInjectNexusServices(services);
-            }
-            else
-            {
-                DependencyInjectBusinessApiServices(services);
-            }
-            DependencyInjectAppServices(services);
-        }
+        /// <summary>
+        /// This is where the application injects its own services.
+        /// </summary>
+        /// <param name="services">From the parameter to Startup.ConfigureServices.</param>
+        /// <remarks>Always override this to inject your services.</remarks>
+        protected abstract void DependencyInjectServices(IServiceCollection services);
         #endregion
 
         #region Configure
@@ -253,8 +232,10 @@ namespace Nexus.Link.Libraries.Web.AspNet.Startup
         /// <see cref="LogRequestAndResponse"/>,
         /// <see cref="ExceptionToFulcrumResponse"/>
         /// </remarks>
-        protected virtual void ConfigureNexusLinkMiddleware(IApplicationBuilder app, IHostingEnvironment env)
+        protected virtual void ConfigureAppMiddleware(IApplicationBuilder app, IHostingEnvironment env)
         {
+            ConfigureSwaggerMiddleware(app, env);
+
             // Get the correlation ID from the request header and store it in FulcrumApplication.Context
             app.UseNexusSaveCorrelationId();
             // Start and stop a batch of logs, see also Nexus.Link.Libraries.Core.Logging.BatchLogger.
@@ -279,17 +260,6 @@ namespace Nexus.Link.Libraries.Web.AspNet.Startup
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", $"{FulcrumApplication.Setup.Name} {ApiVersion}");
             });
         }
-
-        /// <summary>
-        /// Configure optional middleware from the platform.
-        /// </summary>
-        protected abstract void ConfigureBusinessApiMiddleware(IApplicationBuilder app, IHostingEnvironment env);
-
-
-        /// <summary>
-        /// Configure optional middleware unique for this app.
-        /// </summary>
-        protected abstract void ConfigureAppMiddleware(IApplicationBuilder app, IHostingEnvironment env);
         #endregion
 
         /// <inheritdoc />
@@ -297,7 +267,6 @@ namespace Nexus.Link.Libraries.Web.AspNet.Startup
         {
             FulcrumValidate.IsNotNull(TechnicalName, nameof(TechnicalName), errorLocation);
             FulcrumValidate.IsNotNull(ApiVersion, nameof(TechnicalName), ApiVersion);
-            FulcrumValidate.IsNotNull(TechnicalName, nameof(TechnicalName), errorLocation);
         }
     }
 }
