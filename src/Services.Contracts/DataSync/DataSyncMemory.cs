f033466481702cbd6335e70b50937a184ccd85e3
Lars Lindberg <lars.lindberg@xlent.se>
2019-10-02 15:11:49 +0200

    Services.Contracts: 1.4.0 Added IDataSyncTesting, renamed DataSyncMemoryMock.

--- src/Services.Contracts/DataSync/DataSyncMemory.cs
+++ src/Services.Contracts/DataSync/DataSyncMemory.cs
@@ -1,5 +1,8 @@
 ﻿using System.Threading;
 using System.Threading.Tasks;
+using Nexus.Link.Libraries.Core.Application;
+using Nexus.Link.Libraries.Core.Error.Logic;
+using Nexus.Link.Libraries.Core.Storage.Model;
 using Nexus.Link.Libraries.Crud.MemoryStorage;
 using Nexus.Link.Services.Contracts.Events;
 using Nexus.Link.Services.Contracts.Events.SynchronizedEntity;
@@ -9,14 +12,14 @@ namespace Nexus.Link.Services.Contracts.DataSync
     // TODO: This class does not belong in the Services.Contracts library, but it was put here to avoid introducing a new library with only this file.
     /// <inheritdoc cref="IDataSyncCreate{T}" />
     /// <inheritdoc cref="IDataSyncReadUpdate{T}" />
-    public class DataSyncMemoryMock<TModel> : IDataSyncCreate<TModel>, IDataSyncReadUpdate<TModel>
+    public class DataSyncMemory<TModel> : IDataSyncCreate<TModel>, IDataSyncReadUpdate<TModel>, IDataSyncTesting<TModel>
     {
         private readonly string _clientName;
         private readonly string _entityName;
         private readonly CrudMemory<TModel, string> _repository = new CrudMemory<TModel, string>();
 
         /// <inheritdoc />
-        public DataSyncMemoryMock(string clientName, string entityName)
+        public DataSyncMemory(string clientName, string entityName)
         {
             _clientName = clientName;
             _entityName = entityName;
@@ -56,6 +59,27 @@ namespace Nexus.Link.Services.Contracts.DataSync
                 }
             };
             await updatedEvent.PublishAsync(token);
+        }
+
+        /// <inheritdoc />
+        public Task<PageEnvelope<TModel>> ReadAllWithPagingAsync(int offset, int? limit = null, CancellationToken token = default(CancellationToken))
+        {
+            if (FulcrumApplication.IsInProductionOrProductionSimulation) throw new FulcrumNotImplementedException("This method is not expected to run in a production environment");
+            return _repository.ReadAllWithPagingAsync(offset, limit, token);
+        }
+
+        /// <inheritdoc />
+        public Task DeleteAsync(string id, CancellationToken token = default(CancellationToken))
+        {
+            if (FulcrumApplication.IsInProductionOrProductionSimulation) throw new FulcrumNotImplementedException("This method is not expected to run in a production environment");
+            return _repository.DeleteAsync(id, token);
+        }
+
+        /// <inheritdoc />
+        public Task DeleteAllAsync(CancellationToken token = default(CancellationToken))
+        {
+            if (FulcrumApplication.IsInProductionOrProductionSimulation) throw new FulcrumNotImplementedException("This method is not expected to run in a production environment");
+            return _repository.DeleteAllAsync(token);
         }
     }
 }
