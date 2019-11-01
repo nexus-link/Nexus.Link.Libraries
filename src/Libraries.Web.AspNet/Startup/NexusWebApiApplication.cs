4b3dffc79d142270539db460a10f547e1a239213
Magnus Widerberg <Magnus.Widerberg@xlent.se>
2019-11-01 15:29:06 +0100

    Use a StartupHelper class to enable Fundamentals to cleanup App_Data as well.

--- src/Libraries.Web.AspNet/Startup/NexusWebApiApplication.cs
+++ src/Libraries.Web.AspNet/Startup/NexusWebApiApplication.cs
@@ -1,6 +1,4 @@
-﻿
-using System.Configuration;
-#if !NETCOREAPP
+﻿#if !NETCOREAPP
 using System;
 using System.Diagnostics;
 using System.Net;
@@ -54,7 +52,7 @@ namespace Nexus.Link.Libraries.Web.AspNet.Startup
             {
                 ThreadHelper.CallAsyncFromSync(async () => await ApplicationStartBeforeFetchingNexusConfigurationAsync());
 
-                DeleteOldLogFilesFromAppData();
+                StartupHelper.DeleteOldLogFilesFromAppData();
 
                 var configuration = ThreadHelper.CallAsyncFromSync(async () => await FetchConfigurationWithRetriesOnFailAsync());
                 if (configuration == null)
@@ -98,42 +96,6 @@ namespace Nexus.Link.Libraries.Web.AspNet.Startup
             }
 
             return configuration;
-        }
-
-        private static void DeleteOldLogFilesFromAppData()
-        {
-            try
-            {
-                var maxAgeInDays = int.Parse(ConfigurationManager.AppSettings["FallbackLogger.Files.MaxAgeInDays"] ?? "60");
-                var developmentMaxFileSizeInMiB = int.Parse(ConfigurationManager.AppSettings["FallbackLogger.Files.Development.MaxSizeInMebiBytes"] ?? "100");
-
-                var appDataDir = new System.IO.DirectoryInfo($@"{AppDomain.CurrentDomain.BaseDirectory}\App_Data");
-                if (appDataDir.Exists)
-                {
-                    foreach (var fileInfo in appDataDir.GetFiles())
-                    {
-                        if (fileInfo.Name.EndsWith("FulcrumTraceSource.trace.txt"))
-                        {
-                            if (fileInfo.LastWriteTime.AddDays(maxAgeInDays) < DateTime.Now
-                                || FulcrumApplication.IsInDevelopment && fileInfo.Length > developmentMaxFileSizeInMiB * 1024 * 1024)
-                            {
-                                try
-                                {
-                                    fileInfo.Delete();
-                                }
-                                catch (Exception e)
-                                {
-                                    Log.LogWarning($"Could not delete old file {fileInfo.FullName}. LastWriteTime: {fileInfo.LastWriteTime}", e);
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-            catch (Exception e)
-            {
-                Log.LogWarning($"Error during startup deletion of old log files from App_Data", e);
-            }
         }
 
         protected virtual void Application_BeginRequest()
---
+++ src/Libraries.Web.AspNet/Startup/StartupHelper.cs
@@ -0,0 +1,50 @@
+﻿#if !NETCOREAPP
+
+using System;
+using System.Configuration;
+using Nexus.Link.Libraries.Core.Application;
+using Nexus.Link.Libraries.Core.Logging;
+
+namespace Nexus.Link.Libraries.Web.AspNet.Startup
+{
+    public static class StartupHelper
+    {
+        public static void DeleteOldLogFilesFromAppData()
+        {
+            try
+            {
+                var maxAgeInDays = int.Parse(ConfigurationManager.AppSettings["FallbackLogger.Files.MaxAgeInDays"] ?? "60");
+                var developmentMaxFileSizeInMiB = int.Parse(ConfigurationManager.AppSettings["FallbackLogger.Files.Development.MaxSizeInMebiBytes"] ?? "100");
+
+                var appDataDir = new System.IO.DirectoryInfo($@"{AppDomain.CurrentDomain.BaseDirectory}\App_Data");
+                if (appDataDir.Exists)
+                {
+                    foreach (var fileInfo in appDataDir.GetFiles())
+                    {
+                        if (fileInfo.Name.EndsWith("FulcrumTraceSource.trace.txt"))
+                        {
+                            if (fileInfo.LastWriteTime.AddDays(maxAgeInDays) < DateTime.Now
+                                || FulcrumApplication.IsInDevelopment && fileInfo.Length > developmentMaxFileSizeInMiB * 1024 * 1024)
+                            {
+                                try
+                                {
+                                    fileInfo.Delete();
+                                }
+                                catch (Exception e)
+                                {
+                                    Log.LogWarning($"Could not delete old file {fileInfo.FullName}. LastWriteTime: {fileInfo.LastWriteTime}", e);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Log.LogWarning($"Error during startup deletion of old log files from App_Data", e);
+            }
+        }
+    }
+}
+
+#endif
