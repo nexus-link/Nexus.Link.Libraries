48bb72d90359c6f5df1efe31c570bd56558c315c
Magnus Widerberg <Magnus.Widerberg@xlent.se>
2019-12-09 16:15:20 +0100

    Web.AspNet: 2.16.0 Support for ASP.NET WebApi in Translation filter

--- src/Libraries.Web.AspNet/Pipe/Inbound/ValueTranslatorFilter.cs
+++ src/Libraries.Web.AspNet/Pipe/Inbound/ValueTranslatorFilter.cs
@@ -1,26 +1,40 @@
-﻿#if NETCOREAPP
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
 using Nexus.Link.Libraries.Core.Logging;
-using Nexus.Link.Libraries.Web.AspNet.Logging;
 using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Reflection;
+using System.Text;
 using System.Threading;
-using Microsoft.AspNetCore.Mvc.Filters;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.Rest;
 using Nexus.Link.Libraries.Core.Assert;
 using Nexus.Link.Libraries.Core.Translation;
+using Nexus.Link.Libraries.Web.Logging;
+#if NETCOREAPP
+using Nexus.Link.Libraries.Web.AspNet.Logging;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Controllers;
+#else
+using System.Web.Http.Filters;
+using System.Web.Http.Controllers;
+using Newtonsoft.Json.Linq;
+
+#endif
 
 namespace Nexus.Link.Libraries.Web.AspNet.Pipe.Inbound
 {
+#if NETCOREAPP
     public class ValueTranslatorFilter : IAsyncActionFilter, IAsyncResultFilter
+#else
+    public class ValueTranslatorFilter : ActionFilterAttribute
+#endif
     {
         /// <summary>
         /// The service that does the actual translation.
         /// </summary>
-        public ITranslatorService TranslatorService { get;  }
+        public ITranslatorService TranslatorService { get; }
 
         private readonly Func<string> _getClientNameMethod;
 
@@ -32,6 +46,7 @@ namespace Nexus.Link.Libraries.Web.AspNet.Pipe.Inbound
             _getClientNameMethod = getClientNameMethod;
         }
 
+#if NETCOREAPP
         public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
         {
             var translator = GetTranslator(context);
@@ -75,8 +90,51 @@ namespace Nexus.Link.Libraries.Web.AspNet.Pipe.Inbound
 
             await next();
         }
+#else
+        public override Task OnActionExecutingAsync(HttpActionContext actionContext, CancellationToken cancellationToken)
+        {
+            var translator = GetTranslator(actionContext);
+            if (translator != null)
+            {
+                var actionName = actionContext.ActionDescriptor.ActionName;
+                var methodInfo = actionContext.ControllerContext.Controller.GetType()
+                    .GetMethod(actionName, BindingFlags.Public | BindingFlags.Instance);
 
-        private ITranslator GetTranslator(FilterContext context)
+                if (methodInfo != null)
+                {
+                    try
+                    {
+                        DecorateArguments(methodInfo.GetParameters(), actionContext.ActionArguments, translator);
+                    }
+                    catch (Exception exception)
+                    {
+                        LogFailure(actionContext.Request, actionContext.Response, exception);
+                    }
+                }
+            }
+
+            return Task.CompletedTask;
+        }
+
+        public override async Task OnActionExecutedAsync(HttpActionExecutedContext actionExecutedContext, CancellationToken cancellationToken)
+        {
+            var translator = GetTranslator(actionExecutedContext);
+            if (translator != null)
+            {
+                try
+                {
+                    await TranslateResponseAsync(actionExecutedContext, translator, cancellationToken);
+                }
+                catch (Exception exception)
+                {
+                    LogFailure(actionExecutedContext.Request, actionExecutedContext.Response, exception);
+                    throw;
+                }
+            }
+        }
+#endif
+
+        private ITranslator GetTranslator(object context)
         {
             if (context == null || TranslatorService == null) return null;
             var clientName = _getClientNameMethod();
@@ -121,6 +179,8 @@ namespace Nexus.Link.Libraries.Web.AspNet.Pipe.Inbound
             if (currentValue == null) return;
             arguments[parameterName] = translator.Decorate(currentValue);
         }
+
+#if NETCOREAPP
 
         private static void LogDecorationFailure(ActionExecutingContext context, Exception exception)
         {
@@ -190,6 +250,54 @@ namespace Nexus.Link.Libraries.Web.AspNet.Pipe.Inbound
                 $"Failed to translate the response for the request {requestAsLog}. Result:\r{resultAsLog}",
                 exception);
         }
+#else
+        private static void LogFailure(HttpRequestMessage request, HttpResponseMessage response, Exception exception)
+        {
+            string requestAsLog;
+            string resultAsLog;
+
+            try
+            {
+                resultAsLog = response?.Content?.AsString();
+            }
+            catch (Exception e)
+            {
+                resultAsLog = $"Failed to serialize: {e.Message}";
+            }
+
+            try
+            {
+                requestAsLog = request?.ToLogString();
+            }
+            catch (Exception)
+            {
+                requestAsLog = request?.RequestUri.ToString();
+            }
+
+            Log.LogError($"Failed to decorate the arguments for the request {requestAsLog}. Result:\r{resultAsLog}", exception);
+        }
+
+        private static async Task TranslateResponseAsync(HttpActionExecutedContext context, ITranslator translator, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            if (context.ActionContext?.Response?.Content == null) return;
+            var asString = context.ActionContext?.Response?.Content?.AsString();
+            if (string.IsNullOrWhiteSpace(asString)) return;
+
+            try
+            {
+                var objectResult = JObject.Parse(asString);
+                var itemBeforeTranslation = objectResult;
+
+                await translator.Add(itemBeforeTranslation).ExecuteAsync(cancellationToken);
+                var itemAfterTranslation = translator.Translate(itemBeforeTranslation, itemBeforeTranslation.GetType());
+                context.ActionContext.Response.Content = new StringContent(JsonConvert.SerializeObject(itemAfterTranslation), Encoding.UTF8, "application/json");
+
+            }
+            catch (JsonReaderException)
+            {
+                // This is ok; the content might not be JSON
+            }
+        }
+#endif
     }
 }
-#endif
--- test/Nexus.Link.Libraries.Web.AspNet.Tests/InboundPipe/ValueTranslatorFilterUnitTests.cs
+++ test/Nexus.Link.Libraries.Web.AspNet.Tests/InboundPipe/ValueTranslatorFilterUnitTests.cs
@@ -1,20 +1,30 @@
-﻿#if NETCOREAPP
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
 using Nexus.Link.Libraries.Web.AspNet.Tests.InboundPipe.Support;
 using System.Collections.Generic;
 using System.Threading;
 using Nexus.Link.Libraries.Web.AspNet.Pipe.Inbound;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc.Controllers;
-using Microsoft.AspNetCore.Mvc.Filters;
-using Microsoft.AspNetCore.Routing;
 using System.Diagnostics.CodeAnalysis;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using Nexus.Link.Libraries.Core.Application;
 using Nexus.Link.Libraries.Core.Translation;
+
+#if NETCOREAPP
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.AspNetCore.Mvc;
+#else
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+using System.Web.Http;
+using Microsoft.Rest;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+#endif
 
 namespace Nexus.Link.Libraries.Web.AspNet.Tests.InboundPipe
 {
@@ -37,7 +47,8 @@ namespace Nexus.Link.Libraries.Web.AspNet.Tests.InboundPipe
         {
             var inFoo = new Foo
             {
-                Id = "id", Name = "name"
+                Id = "id",
+                Name = "name"
             };
             var json = JsonConvert.SerializeObject(inFoo);
             var outFoo = JsonConvert.DeserializeObject<Foo>(json);
@@ -45,22 +56,25 @@ namespace Nexus.Link.Libraries.Web.AspNet.Tests.InboundPipe
         }
 
         [TestMethod]
-            public async Task ArgumentsAreDecoratedAsync()
-            {
+        public async Task ArgumentsAreDecoratedAsync()
+        {
 
             // Mock a translator
             var testServiceMock = new Mock<ITranslatorService>();
             testServiceMock
-                .Setup(service => service.TranslateAsync(It.IsAny<IEnumerable<string>>(),It.IsAny<string>(), It.IsAny<CancellationToken>()))
-                .ReturnsAsync(() =>new Dictionary<string, string> {{$"(foo.id!~consumer!{Foo.ConsumerId1})", Foo.ProducerId1}});
+                .Setup(service => service.TranslateAsync(It.IsAny<IEnumerable<string>>(), It.IsAny<string>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(() => new Dictionary<string, string> { { $"(foo.id!~consumer!{Foo.ConsumerId1})", Foo.ProducerId1 } });
 
             // Prepare context
-            var foosController = new FoosController();
             var inFoo = new Foo
             {
                 Id = Foo.ConsumerId1,
                 Name = "name"
             };
+            var parameters = new Dictionary<string, object> { { "id", inFoo.Id }, { "item", inFoo } };
+
+#if NETCOREAPP
+            var foosController = new FoosController();
             var controllerActionDescriptor = new ControllerActionDescriptor();
             controllerActionDescriptor.MethodInfo =
                 foosController.GetType().GetMethod(nameof(foosController.UpdateAndReturnAsync));
@@ -71,15 +85,22 @@ namespace Nexus.Link.Libraries.Web.AspNet.Tests.InboundPipe
                 ActionDescriptor = controllerActionDescriptor
             };
             var executingContext = new ActionExecutingContext(actionContext, new List<IFilterMetadata>(),
-                new Dictionary<string, object> {{"id", inFoo.Id}, {"item", inFoo}}, foosController);
+                parameters, foosController);
+#else
+            var contextMock = CreateExecutingContext(parameters, nameof(FoosController.UpdateAndReturnAsync));
+#endif
 
             // Setup the filter
             var filter = new ValueTranslatorFilter(testServiceMock.Object, () => Foo.ConsumerName);
 
             // Run the filter
             Assert.IsFalse(inFoo.Id.StartsWith("(foo.id!"));
+#if NETCOREAPP
             await filter.OnActionExecutionAsync(executingContext, () => Task.FromResult(new ActionExecutedContext(actionContext, new List<IFilterMetadata>(),
                 foosController)));
+#else
+            await filter.OnActionExecutingAsync(contextMock, new CancellationToken());
+#endif
             Assert.IsTrue(inFoo.Id.StartsWith("(foo.id!"));
         }
 
@@ -90,8 +111,8 @@ namespace Nexus.Link.Libraries.Web.AspNet.Tests.InboundPipe
             // Mock a translator
             var testServiceMock = new Mock<ITranslatorService>();
             testServiceMock
-                .Setup(service => service.TranslateAsync(It.IsAny<IEnumerable<string>>(),It.IsAny<string>(), It.IsAny<CancellationToken>()))
-                .ReturnsAsync(() =>new Dictionary<string, string> {{decoratedProducerId1, Foo.ConsumerId1}});
+                .Setup(service => service.TranslateAsync(It.IsAny<IEnumerable<string>>(), It.IsAny<string>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(() => new Dictionary<string, string> { { decoratedProducerId1, Foo.ConsumerId1 } });
 
             // Prepare context
             var foosController = new FoosController();
@@ -100,6 +121,7 @@ namespace Nexus.Link.Libraries.Web.AspNet.Tests.InboundPipe
                 Id = decoratedProducerId1,
                 Name = "name"
             };
+#if NETCOREAPP
             var controllerActionDescriptor = new ControllerActionDescriptor
             {
                 MethodInfo = foosController.GetType().GetMethod(nameof(foosController.UpdateAndReturnAsync))
@@ -112,19 +134,77 @@ namespace Nexus.Link.Libraries.Web.AspNet.Tests.InboundPipe
             };
             var executingContext = new ResultExecutingContext(actionContext, new List<IFilterMetadata>(), new ObjectResult(foo), foosController);
 
+#else
+            var contextMock = CreateExecutedContextWithStatusCode(HttpStatusCode.OK, foo);
+#endif
+
             // Setup the filter
             var filter = new ValueTranslatorFilter(testServiceMock.Object, () => Foo.ConsumerName);
 
             // Run the filter
+#if NETCOREAPP
             await filter.OnResultExecutionAsync(executingContext, () => Task.FromResult(new ResultExecutedContext(actionContext, new List<IFilterMetadata>(), executingContext.Result, foosController)));
+#else
+            await filter.OnActionExecutedAsync(contextMock, new CancellationToken());
+#endif
+
+#if NETCOREAPP
+            var objectResult = executingContext.Result as ObjectResult;
+#else
+            var objectResult = contextMock.Response.Content.AsString();
+#endif
 
             // Verify that the result has been translated
-            var objectResult = executingContext.Result as ObjectResult;
             Assert.IsNotNull(objectResult);
+#if NETCOREAPP
             var outFoo = objectResult.Value as Foo;
+#else
+            var outFoo = JsonConvert.DeserializeObject<Foo>(objectResult);
+#endif
             Assert.IsNotNull(outFoo);
             Assert.AreEqual(Foo.ConsumerId1, outFoo.Id);
         }
+
+#if NETCOREAPP
+#else
+        private HttpActionContext CreateExecutingContext(Dictionary<string, object> actionArguments, string methodName)
+        {
+            var routes = new HttpRouteCollection();
+            var methodInfo = typeof(FoosController).GetMethod(methodName);
+            var context = new HttpActionContext
+            {
+                ControllerContext = new HttpControllerContext
+                {
+                    Request = new HttpRequestMessage(),
+                    Controller = new FoosController()
+                },
+                ActionDescriptor = new ReflectedHttpActionDescriptor(new HttpControllerDescriptor(new HttpConfiguration(routes), nameof(FoosController), typeof(FoosController)), methodInfo)
+            };
+            foreach (var entry in actionArguments)
+            {
+                context.ActionArguments.Add(entry.Key, entry.Value);
+            }
+            return context;
+        }
+
+        private HttpActionExecutedContext CreateExecutedContextWithStatusCode(HttpStatusCode statusCode, object model)
+        {
+            return new HttpActionExecutedContext
+            {
+                ActionContext = new HttpActionContext
+                {
+                    ControllerContext = new HttpControllerContext
+                    {
+                        Request = new HttpRequestMessage()
+                    }
+                },
+                Response = new HttpResponseMessage
+                {
+                    StatusCode = statusCode,
+                    Content = new StringContent(JsonConvert.SerializeObject(model), Encoding.UTF8, "application/json")
+                }
+            };
+        }
+#endif
     }
 }
-#endif
