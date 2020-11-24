352234899d9d7623a449c3a111565bacf7adc8ba
Magnus Widerberg <Magnus.Widerberg@xlent.se>
2020-11-24 08:24:39 +0100

    web.aspnet: 2.19.0-mw-beta-01

--- test/Nexus.Link.Libraries.Web.AspNet.Tests/InboundPipe/NexusTestContextHeaderTest.cs
+++ test/Nexus.Link.Libraries.Web.AspNet.Tests/InboundPipe/NexusTestContextHeaderTest.cs
@@ -1,9 +1,11 @@
-﻿using System.Net;
+﻿using System;
+using System.Net;
 using System.Net.Http;
 using Nexus.Link.Libraries.Web.AspNet.Tests.InboundPipe.Support;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using Nexus.Link.Libraries.Core.Application;
 using Nexus.Link.Libraries.Core.Translation;
@@ -51,6 +53,50 @@ namespace Nexus.Link.Libraries.Web.AspNet.Tests.InboundPipe
             var result = JToken.Parse(resultString).Value<string>();
             Assert.AreEqual(HttpStatusCode.OK, response.StatusCode, result);
             Assert.AreEqual(headerValue, result);
+        }
+
+        [TestMethod]
+        public async Task No_Header_Is_Setup_On_Context()
+        {
+
+#if NETCOREAPP
+            var factory = new CustomWebApplicationFactory();
+            _httpClient = factory.CreateClient();
+#else
+            _httpClient = TestServer.Create<TestStartup>().HttpClient;
+#endif
+
+            var request = new HttpRequestMessage(HttpMethod.Get, "/api/CurrentNexusTestContextValue");
+
+            var response = await _httpClient.SendAsync(request);
+            var resultString = await response.Content.ReadAsStringAsync();
+            var result = JToken.Parse(resultString).Value<string>();
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode, result);
+            Assert.AreEqual("<not setup>", result);
+        }
+
+        [TestMethod]
+        public async Task Json_Value_Can_Be_Used()
+        {
+
+#if NETCOREAPP
+            var factory = new CustomWebApplicationFactory();
+            _httpClient = factory.CreateClient();
+#else
+            _httpClient = TestServer.Create<TestStartup>().HttpClient;
+#endif
+
+            var headerValue = JsonConvert.SerializeObject(new { Id = "123" });
+
+            var request = new HttpRequestMessage(HttpMethod.Get, "/api/CurrentNexusTestContextValue");
+            request.Headers.Add(Constants.NexusTestContextHeaderName, headerValue);
+
+            var response = await _httpClient.SendAsync(request);
+            var resultString = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(resultString);
+            var result = JsonConvert.DeserializeObject<JObject>(JToken.Parse(resultString).ToString());
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode, resultString);
+            Assert.AreEqual(123, result.Value<int>("Id"), resultString);
         }
     }
 }
