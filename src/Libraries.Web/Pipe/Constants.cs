3ff49a2140976ee899330b46089a12ebd424e39f
Magnus Widerberg <Magnus.Widerberg@xlent.se>
2020-11-27 14:42:46 +0100

    X-nexus-test-context header - Core: 4.16.0 Support for FulcrumApplication.Context.NexusTestContext - Web: 2.11.0 Support for propagating header "X-nexus-test-context" in RestClient - Web.AspNet: 2.19.0 Support for setting FulcrumApplication.Context.NexusTestContext from header "X-nexus-test-context"

--- src/Libraries.Web/Pipe/Constants.cs
+++ src/Libraries.Web/Pipe/Constants.cs
@@ -2,6 +2,14 @@
 {
     public class Constants
     {
+        /// <summary>
+        /// Standard correlation id header
+        /// </summary>
         public static string FulcrumCorrelationIdHeaderName = "X-Correlation-ID";
+
+        /// <summary>
+        /// Header to indicate that a request is done in test mode
+        /// </summary>
+        public static string NexusTestContextHeaderName = "X-nexus-test-context";
     }
 }
