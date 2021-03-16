21fb9df620a9774fc37f8af6df6dcb17271eff94
Magnus Widerberg <Magnus.Widerberg@xlent.se>
2021-03-16 12:15:12 +0100

    First stab at verifying a database connection, where we set a ConnectTimeout on open, and then remember the result for a while.

---
+++ src/Libraries.SqlServer/Logic/SqlExtensions.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Caching.Memory;
+using Nexus.Link.Libraries.Core.Error.Logic;
+
+namespace Nexus.Link.Libraries.SqlServer.Logic
+{
+    public static class SqlExtensions
+    {
+        private static MemoryCache _cache = new MemoryCache(new MemoryCacheOptions());
+
+        public static async Task VerifyAvailability(this IDbConnection connection, TimeSpan connectTimeout, TimeSpan considerConnectionBrokenFor, CancellationToken cancellationToken = default)
+        {
+            if (connection.State == ConnectionState.Open) return;
+
+            var originalConnectionString = connection.ConnectionString;
+            var cacheKey = $"{nameof(SqlExtensions)}/{originalConnectionString}";
+            _cache.TryGetValue(cacheKey, out DateTimeOffset? timestamp);
+
+            var sqlConnection = connection as SqlConnection;
+
+            try
+            {
+                if (timestamp != null) throw new ApplicationException($"This connection is considered bad since {timestamp:O}");
+
+                var connStringBuilder = new SqlConnectionStringBuilder(connection.ConnectionString)
+                {
+                    ConnectTimeout = (int) connectTimeout.TotalSeconds
+                };
+                try
+                {
+                    connection.ConnectionString = connStringBuilder.ConnectionString;
+                    if (sqlConnection != null) await sqlConnection.OpenAsync(cancellationToken);
+                    else connection.Open();
+                }
+                finally
+                {
+                    connection.ConnectionString = originalConnectionString;
+                }
+            }
+            catch (Exception e)
+            {
+                if (timestamp == null)
+                {
+                    _cache.Set(cacheKey, DateTimeOffset.Now, DateTimeOffset.Now.Add(considerConnectionBrokenFor));
+                }
+
+                var dataSource = sqlConnection?.DataSource ?? "(unknown data source)";
+                throw new FulcrumResourceException($"Could not open database connection to {connection.Database} on {dataSource}", e);
+            }
+        }
+
+        public static void ResetCache()
+        {
+            _cache = new MemoryCache(new MemoryCacheOptions());
+        }
+    }
+}
---
+++ test/Nexus.Link.Libraries.SqlServer.Test/SqlExtensionsTests.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Data;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Nexus.Link.Libraries.Core.Error.Logic;
+using Nexus.Link.Libraries.SqlServer.Logic;
+
+namespace Nexus.Link.Libraries.SqlServer.Test
+{
+    [TestClass]
+    public class SqlExtensionsTests
+    {
+        private Mock<IDbConnection> _connectionMock;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            SqlExtensions.ResetCache();
+            _connectionMock = new Mock<IDbConnection>();
+            _connectionMock.SetupProperty(x => x.ConnectionString);
+            _connectionMock.Object.ConnectionString = "Server=localhost;Database=mock-database;";
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FulcrumResourceException))]
+        public async Task Throws_ResourceException_When_Open_Connection_Throws_Exception()
+        {
+            _connectionMock.Setup(x => x.Open()).Throws(new ApplicationException("unavailable"));
+
+            await _connectionMock.Object.VerifyAvailability(TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(60));
+        }
+
+        [TestMethod]
+        public async Task Breaks_Circuit_On_Consecutive_Invocations()
+        {
+            var innerException = new ApplicationException("unavailable");
+            _connectionMock.Setup(x => x.Open()).Throws(innerException).Verifiable();
+
+            try
+            {
+                await _connectionMock.Object.VerifyAvailability(TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(60));
+                Assert.Fail("Verify should throw (1)");
+            }
+            catch (FulcrumResourceException e)
+            {
+                _connectionMock.Verify(x => x.Open(), Times.Once);
+                Assert.AreEqual(innerException, e.InnerException);
+            }
+            try
+            {
+                await _connectionMock.Object.VerifyAvailability(TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(60));
+                Assert.Fail("Verify should throw (2)");
+            }
+            catch (FulcrumResourceException e2)
+            {
+                _connectionMock.Verify(x => x.Open(), Times.Once);
+                Assert.AreNotEqual(innerException, e2.InnerException);
+            }
+        }
+
+        // TODO: Test fail fast (not calling Open())
+        // TODO: performance test
+
+    }
+}
