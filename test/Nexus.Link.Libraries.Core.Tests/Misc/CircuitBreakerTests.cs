ec161d8446f8d640b30bdb661e5cce12cf4a05cc
Lars Lindberg <lars.lindberg@xlent.se>
2021-03-29 21:28:58 +0200

    Renamed test file.

---
+++ test/Nexus.Link.Libraries.Core.Tests/Misc/CircuitBreakerTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Nexus.Link.Libraries.Core.Health.Logic;
+using Nexus.Link.Libraries.Core.Health.Model;
+using Nexus.Link.Libraries.Core.MultiTenant.Model;
+
+namespace Nexus.Link.Libraries.Core.Tests.Misc
+{
+    [TestClass]
+    public class CircuitBreakerTests
+    {
+        [TestInitialize]
+        public void Initialize()
+        {
+        }
+
+        [TestMethod]
+        public async Task TestWithIResourceHealth2()
+        {
+        }
+    }
+}
