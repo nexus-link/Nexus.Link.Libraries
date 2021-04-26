a912f1675ddad012bb76a558473932480d9c5baf
Lars Lindberg <lars.lindberg@xlent.se>
2021-04-26 16:14:43 +0200

    Added synchronous circuit breaker in SqlExtensions.

--- src/Libraries.SqlServer/Logic/SqlExtensions.cs
+++ src/Libraries.SqlServer/Logic/SqlExtensions.cs
@@ -68,5 +68,51 @@ namespace Nexus.Link.Libraries.SqlServer.Logic
                 if (connection.State != ConnectionState.Open) connection.ConnectionString = originalConnectionString;
             }
         }
+
+        /// <summary>
+        /// Verify that the database connection is available by trying to open it.
+        /// </summary>
+        /// <param name="connection">The database connection.</param>
+        /// <param name="connectTimeout">The time that we are willing to wait for the DB response.
+        /// NULL means that we should use the value from the connection string.</param>
+        /// <returns></returns>
+        public static void VerifyAvailability(this IDbConnection connection, TimeSpan? connectTimeout = null)
+        {
+            if (connection.State == ConnectionState.Open) return;
+
+            CircuitBreakerCollection.ExecuteOrThrow(
+                connection.ConnectionString,
+                () => Connect(connection, connectTimeout));
+        }
+
+        private static void Connect(IDbConnection connection, TimeSpan? connectTimeout)
+        {
+            var originalConnectionString = connection.ConnectionString;
+            try
+            {
+                if (connectTimeout.HasValue)
+                {
+                    var connStringBuilder = new SqlConnectionStringBuilder(connection.ConnectionString)
+                    {
+                        ConnectTimeout = (int) connectTimeout.Value.TotalSeconds
+                    };
+                    connection.ConnectionString = connStringBuilder.ConnectionString;
+                }
+
+                connection.Open();
+            }
+            catch (Exception e)
+            {
+                var sqlConnection = connection as SqlConnection;
+                var database = connection.Database ?? "(unknown database)";
+                var dataSource = sqlConnection?.DataSource ?? "(unknown data source)";
+                var exception = new FulcrumResourceException($"Could not open database connection to {database} on {dataSource}", e);
+                throw new CircuitBreakerException(exception);
+            }
+            finally
+            {
+                if (connection.State != ConnectionState.Open) connection.ConnectionString = originalConnectionString;
+            }
+        }
     }
 }
--- test/Nexus.Link.Libraries.SqlServer.Test/SqlExtensionsWithRealCircuitBreakerTests.cs
+++ test/Nexus.Link.Libraries.SqlServer.Test/SqlExtensionsWithRealCircuitBreakerTests.cs
@@ -32,7 +32,7 @@ namespace Nexus.Link.Libraries.SqlServer.Test
 
         [TestMethod]
         [ExpectedException(typeof(FulcrumResourceException))]
-        public async Task Throws_ResourceException_When_Open_Connection_Throws_Exception()
+        public async Task Throws_ResourceException_When_Open_Connection_Throws_Exception_Async()
         {
             _connectionMock.Setup(x => x.Open()).Throws(new ApplicationException("unavailable"));
 
@@ -40,7 +40,16 @@ namespace Nexus.Link.Libraries.SqlServer.Test
         }
 
         [TestMethod]
-        public async Task Breaks_Circuit_On_Consecutive_Invocations()
+        [ExpectedException(typeof(FulcrumResourceException))]
+        public void Throws_ResourceException_When_Open_Connection_Throws_Exception_Sync()
+        {
+            _connectionMock.Setup(x => x.Open()).Throws(new ApplicationException("unavailable"));
+
+            _connectionMock.Object.VerifyAvailability(TimeSpan.FromSeconds(1));
+        }
+
+        [TestMethod]
+        public async Task Breaks_Circuit_On_Consecutive_Invocations_Async()
         {
             var innerException = new ApplicationException("unavailable");
             _connectionMock
@@ -85,7 +94,52 @@ namespace Nexus.Link.Libraries.SqlServer.Test
         }
 
         [TestMethod]
-        public async Task Recovers_After_Success()
+        public void Breaks_Circuit_On_Consecutive_Invocations_Sync()
+        {
+            var innerException = new ApplicationException("unavailable");
+            _connectionMock
+                .Setup(x => x.Open())
+                .Callback(() => Task.Delay(200).Wait())
+                .Throws(innerException)
+                .Verifiable();
+
+            // Fail once to set the fail state
+            try
+            {
+                _connectionMock.Object.VerifyAvailability(TimeSpan.FromSeconds(1));
+                Assert.Fail("Verify should throw (1)");
+            }
+            catch (FulcrumResourceException e)
+            {
+                _connectionMock.Verify(x => x.Open(), Times.Once);
+                Assert.AreEqual(innerException, e.InnerException);
+            }
+
+            // Load test
+            var resetEvent = new ManualResetEvent(false);
+            var count = 0;
+            const int parallelCalls = 1000;
+
+            Parallel.For(0, parallelCalls, i =>
+            {
+                try
+                {
+                    _connectionMock.Object.VerifyAvailability(TimeSpan.FromSeconds(1));
+                    Assert.Fail("Verify should throw (2)");
+                }
+                catch (FulcrumResourceException)
+                {
+                    _connectionMock.Verify(x => x.Open(), Times.Once);
+                    if (Interlocked.Increment(ref count) >= parallelCalls) resetEvent.Set();
+                }
+            });
+
+            Assert.IsTrue(resetEvent.WaitOne(TimeSpan.FromSeconds(1)), "A small time span should be enough to wait for all calls");
+            Assert.AreEqual(parallelCalls, count);
+        }
+
+        [TestMethod]
+        public async Task Recovers_After_Success_Async()
         {
             // 1. Fail
             _connectionMock.Setup(x => x.Open()).Throws(new ApplicationException("unavailable"));
@@ -115,6 +169,40 @@ namespace Nexus.Link.Libraries.SqlServer.Test
             _connectionMock.Setup(x => x.Open()).Verifiable();
 
             await _connectionMock.Object.VerifyAvailabilityAsync(TimeSpan.FromSeconds(1));
+            _connectionMock.Verify(x => x.Open(), Times.Exactly(2), "Open() should have been executed again after recovery");
+        }
+
+        [TestMethod]
+        public void Recovers_After_Success_Sync()
+        {
+            // 1. Fail
+            _connectionMock.Setup(x => x.Open()).Throws(new ApplicationException("unavailable"));
+            try
+            {
+                _connectionMock.Object.VerifyAvailability(TimeSpan.FromSeconds(1));
+                Assert.Fail("Verify should throw");
+            }
+            catch (FulcrumResourceException)
+            {
+                _connectionMock.Verify(x => x.Open(), Times.Once, "Open() should have been executed");
+            }
+
+            // 2. Verify failed state
+            try
+            {
+                _connectionMock.Object.VerifyAvailability(TimeSpan.FromSeconds(1));
+                Assert.Fail("Verify should throw");
+            }
+            catch (FulcrumResourceException)
+            {
+                _connectionMock.Verify(x => x.Open(), Times.Once, "Open() should NOT have been executed");
+            }
+
+            // 3. Recover
+            SqlExtensions.CircuitBreakerCollection.ForceEndOfCoolDown();
+            _connectionMock.Setup(x => x.Open()).Verifiable();
+
+            _connectionMock.Object.VerifyAvailability(TimeSpan.FromSeconds(1));
             _connectionMock.Verify(x => x.Open(), Times.Exactly(2), "Open() should have been executed again after recovery");
         }
     }
