22e7340b69f7a5579d02b8943248c514b7b6c9e4
Lars Lindberg <lars.lindberg@xlent.se>
2021-04-29 16:57:09 +0200

    Missing files

---
+++ src/Libraries.Crud/Interfaces/ITransactionLock.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Nexus.Link.Libraries.Core.Error.Logic;
+using Nexus.Link.Libraries.Crud.Model;
+
+namespace Nexus.Link.Libraries.Crud.Interfaces
+{
+    /// <summary>
+    /// Lock/unlock an item.
+    /// </summary>
+    /// <typeparam name="TId">The type for the id parameter.</typeparam>
+    public interface ITransactionLock<in TId> : ICrudable<TId>
+    {
+        /// <summary>
+        /// Claim a lock for the item with id <paramref name="id"/>
+        /// </summary>
+        /// <param name="id">How the object to be locked is identified.</param>
+        /// <param name="token">Propagates notification that operations should be canceled</param>
+        /// <exception cref="FulcrumTryAgainException">
+        /// Thrown if there already is a claimed lock.
+        /// </exception>
+        /// <remarks>
+        /// The lock will be automatically released when the transaction has completed or been aborted.
+        /// </remarks>
+        Task ClaimTransactionLockAsync(TId id, CancellationToken token = default(CancellationToken));
+    }
+}
---
+++ src/Libraries.Crud/Interfaces/ITransactionLockSlave.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Nexus.Link.Libraries.Core.Error.Logic;
+using Nexus.Link.Libraries.Crud.Model;
+
+namespace Nexus.Link.Libraries.Crud.Interfaces
+{
+    /// <summary>
+    /// Lock/unlock an item.
+    /// </summary>
+    /// <typeparam name="TId">The type for the id parameter.</typeparam>
+    public interface ITransactionLockSlave<in TId> : ICrudable<TId>
+    {
+        /// <summary>
+        /// Claim a lock for the item uniquely identified by <paramref name="masterId"/> and <paramref name="slaveId"/>
+        /// </summary>
+        /// <param name="masterId">The id for the master object.</param>
+        /// <param name="slaveId">The id for the slave object.</param>
+        /// <param name="token">Propagates notification that operations should be canceled</param>
+        /// <exception cref="FulcrumTryAgainException">
+        /// Thrown if there already is a claimed lock. Will contain information about when the lock is automatically released.
+        /// </exception>
+        /// <remarks>
+        /// The lock will be automatically released when the transaction has completed or been aborted.
+        /// </remarks>
+        Task ClaimTransactionLockAsync(TId masterId, TId slaveId, CancellationToken token = default(CancellationToken));
+
+    }
+}
