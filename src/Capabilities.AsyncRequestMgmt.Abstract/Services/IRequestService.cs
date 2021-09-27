08fc5274eb20c6af4deb85bf2e68f38c57aba30f
Lars Lindberg <lars.lindberg@xlent.se>
2021-09-27 10:21:02 +0200

    Capabilities.AsyncRequestMgmt.Abstract: 1.0.1 Removed reduntant service operation.

--- src/Capabilities.AsyncRequestMgmt.Abstract/Services/IRequestService.cs
+++ src/Capabilities.AsyncRequestMgmt.Abstract/Services/IRequestService.cs
@@ -10,9 +10,5 @@ namespace Nexus.Link.Capabilities.AsyncRequestMgmt.Abstract.Services
     /// </summary>
     public interface IRequestService : ICreate<HttpRequestCreate, HttpRequest, string>
     {
-        /// <summary>
-        /// To read the response. for the corresponding request
-        /// </summary>
-        Task<HttpResponse> ReadResponseAsync(string requestId, CancellationToken token = default);
     }
 }
