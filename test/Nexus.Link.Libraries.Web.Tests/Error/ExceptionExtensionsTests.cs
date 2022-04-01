497b3d5e10c4b52621d4674d60175a72f53aac36
Lars Lindberg <lars.lindberg@xlent.se>
2022-04-01 13:17:54 +0200

    Web: 2.25.0 Added extension Task.FireAndForgetAsync()

---
+++ test/Nexus.Link.Libraries.Web.Tests/Error/ExceptionExtensionsTests.cs
@@ -0,0 +1,47 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Nexus.Link.Libraries.Core.Application;
+using Nexus.Link.Libraries.Core.Error.Logic;
+using Nexus.Link.Libraries.Core.Error.Model;
+using Nexus.Link.Libraries.Web.Error.Logic;
+using Shouldly;
+
+namespace Nexus.Link.Libraries.Web.Tests.Error
+{
+    [TestClass]
+    public class ExceptionExtensionsTests
+    {
+        [TestInitialize]
+        public void RunBeforeEachTestMethod()
+        {
+            FulcrumApplicationHelper.UnitTestSetup(typeof(ExceptionConverterTests).FullName);
+        }
+
+        [TestMethod]
+        public async Task FireAndForget_01_Given_RequestAcceptedException_IgnoresException()
+        {
+            await Task.FromException(new RequestAcceptedException("a"))
+                .FireAndForgetAsync();
+        }
+
+        [TestMethod]
+        public async Task FireAndForget_01_Given_OtherException_RethrowsException()
+        {
+            await Task.FromException(new FulcrumTryAgainException("a"))
+                .FireAndForgetAsync()
+                .ShouldThrowAsync<FulcrumTryAgainException>();
+        }
+
+        [TestMethod]
+        public async Task FireAndForget_01_Given_OtherExceptionAndIgnoreAll_SwallowsException()
+        {
+            await Task.FromException(new FulcrumTryAgainException("a"))
+                .FireAndForgetAsync(true);
+        }
+    }
+}
