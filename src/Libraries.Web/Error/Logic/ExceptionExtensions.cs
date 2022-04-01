0bff4fd920d20ad817198769dcc5f80a6269b46a
Lars Lindberg <lars.lindberg@xlent.se>
2022-04-01 14:04:16 +0200

    Added overload of FireAndForgetAsync

--- src/Libraries.Web/Error/Logic/ExceptionExtensions.cs
+++ src/Libraries.Web/Error/Logic/ExceptionExtensions.cs
@@ -30,5 +30,27 @@ namespace Nexus.Link.Libraries.Web.Error.Logic
                 Log.LogVerbose($"{nameof(FireAndForgetAsync)} silently ignored exception {e}", e);
             }
         }
+
+        /// <summary>
+        /// Use this for calls to services that can throw <see cref="RequestAcceptedException"/>.
+        /// If the service accepts the request, the exception is silently ignored.
+        /// There is also an option to set <paramref name="ignoreAllExceptions"/>, where all
+        /// exceptions are silently ignored.
+        /// </summary>
+        /// <param name="task">The task that can throw a <see cref="RequestAcceptedException"/></param>
+        /// <param name="ignoreAllExceptions">If this is true, then all exceptions are silently ignored.</param>
+        public static async Task FireAndForgetAsync<T>(this Task<T> task, bool ignoreAllExceptions = false)
+        {
+            try
+            {
+                await task;
+            }
+            catch (Exception e)
+            {
+                var requestWasAccepted = (e is RequestAcceptedException);
+                if (!requestWasAccepted && !ignoreAllExceptions) throw;
+                Log.LogVerbose($"{nameof(FireAndForgetAsync)} silently ignored exception {e}", e);
+            }
+        }
     }
 }
--- test/Nexus.Link.Libraries.Web.Tests/Error/ExceptionExtensionsTests.cs
+++ test/Nexus.Link.Libraries.Web.Tests/Error/ExceptionExtensionsTests.cs
@@ -1,13 +1,8 @@
-﻿using System.Net;
-using System.Net.Http;
-using System.Text;
+﻿using System;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 using Nexus.Link.Libraries.Core.Application;
 using Nexus.Link.Libraries.Core.Error.Logic;
-using Nexus.Link.Libraries.Core.Error.Model;
 using Nexus.Link.Libraries.Web.Error.Logic;
 using Shouldly;
 
@@ -25,23 +20,57 @@ namespace Nexus.Link.Libraries.Web.Tests.Error
         [TestMethod]
         public async Task FireAndForget_01_Given_RequestAcceptedException_IgnoresException()
         {
-            await Task.FromException(new RequestAcceptedException("a"))
-                .FireAndForgetAsync();
+            await AlwaysThrowAsync(new FulcrumTryAgainException("m"))
+                .ShouldThrowAsync < FulcrumTryAgainException>();
+            //await (AlwaysThrowAsync(new RequestAcceptedException("a"))
+            //    .FireAndForgetAsync());
         }
 
         [TestMethod]
-        public async Task FireAndForget_01_Given_OtherException_RethrowsException()
+        public async Task FireAndForget_02_Given_OtherException_RethrowsException()
         {
-            await Task.FromException(new FulcrumTryAgainException("a"))
+            await AlwaysThrowAsync(new FulcrumTryAgainException("a"))
                 .FireAndForgetAsync()
                 .ShouldThrowAsync<FulcrumTryAgainException>();
         }
 
         [TestMethod]
-        public async Task FireAndForget_01_Given_OtherExceptionAndIgnoreAll_SwallowsException()
+        public async Task FireAndForget_03_Given_OtherExceptionAndIgnoreAll_SwallowsException()
         {
-            await Task.FromException(new FulcrumTryAgainException("a"))
+            await AlwaysThrowAsync(new FulcrumTryAgainException("a"))
                 .FireAndForgetAsync(true);
+        }
+
+        [TestMethod]
+        public async Task FireAndForget2_01_Given_RequestAcceptedException_IgnoresException()
+        {
+            await AlwaysThrowAsync(1, new RequestAcceptedException("a"))
+                .FireAndForgetAsync();
+        }
+
+        [TestMethod]
+        public async Task FireAndForget2_02_Given_OtherException_RethrowsException()
+        {
+            await AlwaysThrowAsync(1, new FulcrumTryAgainException("a"))
+                .FireAndForgetAsync()
+                .ShouldThrowAsync<FulcrumTryAgainException>();
+        }
+
+        [TestMethod]
+        public async Task FireAndForget2_03_Given_OtherExceptionAndIgnoreAll_SwallowsException()
+        {
+            await AlwaysThrowAsync(1, new FulcrumTryAgainException("a"))
+                .FireAndForgetAsync(true);
+        }
+
+        private Task<int> AlwaysThrowAsync(int i, Exception e)
+        {
+            return Task.FromException<int>(e);
+        }
+
+        private Task AlwaysThrowAsync(Exception e)
+        {
+            return Task.FromException(e);
         }
     }
 }
