2d6ed6a439d3d972de1bf01b1a054546c94e2c39
Lars Lindberg <lars.lindberg@xlent.se>
2022-08-30 17:50:55 +0200

    Crud: 3.14.0 Added FileSystem

---
+++ src/Libraries.Crud/FileSystem/IFileSystemService.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Nexus.Link.Libraries.Crud.Interfaces;
+
+namespace Nexus.Link.Libraries.Crud.FileSystem;
+
+/// <summary>
+/// A service for dealing with <see cref="FileOrDirectory"/>.
+/// </summary>
+public interface IFileSystemService : 
+    ICreateWithSpecifiedId<FileOrDirectory, string>,
+    IRead<FileOrDirectory, string>,
+    IReadChildren<FileOrDirectory, string>,
+    IDeleteChildren<string>
+{
+    Task<bool> ExistsAsync(string id, CancellationToken cancellationToken = default);
+
+    Task<TextWriter> CreateTextWriterAsync(string id);
+    Task<string> ReadContentAsStringAsync(FileOrDirectory file);
+}
