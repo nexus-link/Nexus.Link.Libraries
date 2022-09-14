009588b8340b7da99308885108566939f741cb02
Lars Lindberg <lars.lindberg@xlent.se>
2022-09-14 21:08:05 +0200

    Core: 5.24.0 Added StringExtensions with RemovePrefix() and RemoveSuffix()

---
+++ src/Libraries.Core/Misc/StringExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Nexus.Link.Libraries.Core.Misc
+{
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Remove <paramref name="prefix"/> from the start of <paramref name="longString"/>, using <paramref name="stringComparison"/>.
+        /// </summary>
+        public static string RemovePrefix(this string longString, string prefix, StringComparison stringComparison = StringComparison.CurrentCulture)
+        {
+            if (prefix == null) return longString;
+            if (longString == null || longString.Length < prefix.Length) return null;
+            if (!longString.StartsWith(prefix, stringComparison)) return longString;
+            return longString.Substring(prefix.Length);
+        }
+
+        /// <summary>
+        /// Remove <paramref name="suffix"/> from the end of <paramref name="longString"/>, using <paramref name="stringComparison"/>.
+        /// </summary>
+        public static string RemoveSuffix(this string longString, string suffix, StringComparison stringComparison = StringComparison.CurrentCulture)
+        {
+            if (suffix == null) return longString;
+            if (longString == null || longString.Length < suffix.Length) return null;
+            if (!longString.EndsWith(suffix, stringComparison)) return longString;
+            return longString.Substring(0, longString.Length-suffix.Length);
+        }
+    }
+}
---
+++ test/Nexus.Link.Libraries.Core.Tests/Misc/StringExtensionsTests.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Nexus.Link.Libraries.Core.Misc;
+using Shouldly;
+using System;
+
+namespace Nexus.Link.Libraries.Core.Tests.Misc
+{
+    [TestClass]
+    public class StringExtensionsTests
+    {
+
+        [TestInitialize]
+        public void Initialize()
+        {
+        }
+
+        [DataTestMethod]
+        [DataRow(null, null, null)]
+        [DataRow("prefix", null, null)]
+        [DataRow(null, "long string", "long string")]
+        [DataRow("ef", "abcdef", "abcdef")]
+        [DataRow("a", "abcdef", "bcdef")]
+        [DataRow("ab", "abcdef", "cdef")]
+        [DataRow("abcdef", "abcdef", "")]
+        public void RemovePrefix_DefaultStringComparison(string prefix, string longString, string expectedResult)
+        {
+            var actualResult = longString.RemovePrefix(prefix);
+            actualResult.ShouldBe(expectedResult);
+        }
+
+        [DataTestMethod]
+        [DataRow("A", "abc", StringComparison.InvariantCulture, "abc")]
+        [DataRow("a", "ABC", StringComparison.InvariantCulture, "ABC")]
+        [DataRow("A", "abc", StringComparison.InvariantCultureIgnoreCase, "bc")]
+        [DataRow("a", "ABC", StringComparison.InvariantCultureIgnoreCase, "BC")]
+        [DataRow("Å", "åäö", StringComparison.InvariantCulture, "åäö")]
+        [DataRow("å", "ÅÄÖ", StringComparison.InvariantCulture, "ÅÄÖ")]
+        [DataRow("Å", "åäö", StringComparison.InvariantCultureIgnoreCase, "äö")]
+        [DataRow("å", "ÅÄÖ", StringComparison.InvariantCultureIgnoreCase, "ÄÖ")]
+        [DataRow("Å", "åäö", StringComparison.CurrentCulture, "åäö")]
+        [DataRow("å", "ÅÄÖ", StringComparison.CurrentCulture, "ÅÄÖ")]
+        [DataRow("Å", "åäö", StringComparison.CurrentCultureIgnoreCase, "äö")]
+        [DataRow("å", "ÅÄÖ", StringComparison.CurrentCultureIgnoreCase, "ÄÖ")]
+        public void RemovePrefix_StringComparisons(string prefix, string longString, StringComparison stringComparison, string expectedResult)
+        {
+            var actualResult = longString.RemovePrefix(prefix, stringComparison);
+            actualResult.ShouldBe(expectedResult);
+        }
+
+        [DataTestMethod]
+        [DataRow(null, null, null)]
+        [DataRow(null, "suffix", null)]
+        [DataRow("long string", null, "long string")]
+        [DataRow("abcdef", "ab", "abcdef")]
+        [DataRow("abcdef", "f", "abcde")]
+        [DataRow("abcdef", "ef", "abcd")]
+        [DataRow("abcdef", "abcdef", "")]
+        public void RemoveSuffix_DefaultComparison(string longString, string suffix, string expectedResult)
+        {
+            var actualResult = longString.RemoveSuffix(suffix);
+            actualResult.ShouldBe(expectedResult);
+        }
+    };
+}
